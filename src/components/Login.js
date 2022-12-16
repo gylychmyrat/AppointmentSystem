b2200f7250f22c62f817854b0877735fe323c0d1
gylychmyrat <120544679+gylychmyrat@users.noreply.github.com>
2022-12-16 11:58:51 -0600

    Update Login.js
    CleanUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -38,10 +38,7 @@ export default function Login() {
     const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    
   };
 
   const userLogin=()=>{
